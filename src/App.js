86820c49a23b4f4d2a487f9696aa559cc0673969
Luhae <julianajnd@gmail.com>
2023-09-28 17:32:34 -0300

    bootstrap and header added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header.js';
 
 function App() {
   return (
+    <>
+    <Header/>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Liga Grand Line!
         </p>
       </header>
     </div>
+    </>
   );
 }
 
